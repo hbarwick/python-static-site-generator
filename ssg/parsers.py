22be771a9902600e53ff6937c62218114f2a3948
Hallam Barwick <hallam.barwick@assurant.com>
2023-01-13 15:05:06 +0000

    Module 2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,11 +11,11 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path: Path):
-        with open(path) as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path: Path, dest: Path, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path) as file:
+        with open(full_path, "w") as file:
             file.write(content)
 
